0350b5ece00108b6b3d876f1dc36f2334bb6dad7
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-04 14:58:41 -0300

    feat: user Pages

---
+++ frontend/my-app/src/pages/UserPage/index.tsx
@@ -0,0 +1,11 @@
+function User() {
+    return (
+        <>
+            {/* <NavBar /> */}
+            <h1>UserPage</h1>
+            {/* <Footer /> */}
+        </>
+    );
+}
+
+export default User;
---
+++ frontend/my-app/src/types/user.ts
@@ -0,0 +1,5 @@
+export type user = {
+    username: string;
+    displayname: string;
+    password: string;
+}
