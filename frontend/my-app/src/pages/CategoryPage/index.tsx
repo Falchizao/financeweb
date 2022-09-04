e3d5543d0aa773549df89d9780af6cd1adbf38a7
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-04 14:58:17 -0300

    feat: category pages

---
+++ frontend/my-app/src/pages/CategoryPage/index.tsx
@@ -0,0 +1,11 @@
+function Category() {
+    return (
+        <>
+            {/* <NavBar /> */}
+            <h1>CategoryPage</h1>
+            {/* <Footer /> */}
+        </>
+    );
+}
+
+export default Category;
---
+++ frontend/my-app/src/types/category.ts
@@ -0,0 +1,3 @@
+export type category = {
+    name: string;
+}
