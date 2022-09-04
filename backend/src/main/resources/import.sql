7c28428333f25cc3d3449e879f1ef5054061675a
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-04 14:28:42 -0300

    feat: auto sql import

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_user(username,displayName,password) VALUES ('Marcelo Falchi', 'Falchi', '12345');
+INSERT INTO tb_user(username,displayName,password) VALUES ('Vini Pegorini', 'Pegorini', '1234');
+INSERT INTO tb_user(username,displayName,password) VALUES ('Ricardo Braga', 'Braga','123456');
+INSERT INTO tb_user(username,displayName,password) VALUES ('Ricardo Solares', 'Solares','1234567');
+
+
