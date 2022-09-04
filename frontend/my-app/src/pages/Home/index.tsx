1ccae4cc17cffb26190e5d325334d00adf8dae39
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-04 14:58:50 -0300

    feat: home page

---
+++ frontend/my-app/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+function Home() {
+    return (
+        <>
+            {/* <NavBar /> */}
+            <h1>HomePage</h1>
+            {/* <Footer /> */}
+        </>
+    );
+}
+
+export default Home;
