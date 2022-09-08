27523e86d39238e15a011ad039217804f27ba1f5
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-08 19:42:47 -0300

    feat: footer

---
+++ frontend/my-app/src/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+function Footer(){
+    return(
+        <div className="page-footer font-small bg-dark">
+            <div className="text-center py-3 text-white">
+                <p>2022 Falchi</p>
+                <p>FinanceWeb</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
