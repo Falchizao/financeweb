4710214e949aa479955b409e98a0b45d512938ee
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-10 23:16:12 -0300

    chore: components props to axios request

--- frontend/my-app/src/components/deleteButton/index.tsx
+++ frontend/my-app/src/components/deleteButton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios'
+import { toast } from 'react-toastify'
 import addbtn from '../../assets/img/icon-delete.svg'
+import { BASE_URL } from '../../utils/requests'
 import './styles.css'
 
-function DeleteButton(){
+type Props = {
+    id: number
+}
+
+function handleClick(deletionID : number){
+    axios.delete(`${BASE_URL}/api`)
+        .then(response => {
+            toast.info("Deleted with success");
+        });
+}
+
+function DeleteButton( {id} : Props ){
     return(
-        <div className="falchi-del-btn">
+        <div className="falchi-del-btn" onClick={() => handleClick(id)}>
             <img src={addbtn} alt="Delete" />    
         </div>
     )
--- frontend/my-app/src/components/editbutton/index.tsx
+++ frontend/my-app/src/components/editbutton/index.tsx
@@ -1,9 +1,23 @@
+import axios from 'axios';
+import { toast } from 'react-toastify';
 import editbtn from '../../assets/img/icon-edit.svg'
+import { BASE_URL } from '../../utils/requests';
 import './styles.css'
 
-function EditButton(){
+type Props = {
+    id: number
+}
+
+function handleClick(editID : number){
+    axios.put(`${BASE_URL}/api/${id}`)
+        .then(response => {
+            toast.info("Deleted with success");
+        });
+}
+
+function EditButton( {id} : Props){
     return(
-        <div className="falchi-edit-btn">
+        <div className="falchi-edit-btn" onClick={() => handleClick(id)}>
             <img src={editbtn} alt="Edit" />    
         </div>
     )
