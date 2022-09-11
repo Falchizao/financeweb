aea45ce0c2fd0b8366333067b7f1b12e8c17ee83
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-10 23:13:08 -0300

    chore: ToastContainer

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,10 +1,15 @@
 import WebRoutes from "./Routes";
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
+    <>
+    <ToastContainer />
     <WebRoutes />
+    </>
   );
 }
 
