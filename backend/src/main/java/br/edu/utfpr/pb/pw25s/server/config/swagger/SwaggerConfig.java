c03bac335df085b0810ca0a43b36f35a1f5cb69d
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-09-18 23:11:29 -0300

    Feat: Swagger API Documentation

--- backend/src/main/java/br/edu/utfpr/pb/pw25s/server/config/swagger/SwaggerConfig.java
+++ backend/src/main/java/br/edu/utfpr/pb/pw25s/server/config/swagger/SwaggerConfig.java
@@ -11,8 +11,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.pines.dev"))
-                .paths(PathSelectors.ant("/**"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 }
