cb31a96292ef05f22f9fc80cf6ebaceba559a464
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-10-02 18:10:31 -0300

    feat: add category page

---
+++ frontend/my-app/src/components/add/addcategory/index.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { NavigateFunction, useNavigate } from 'react-router-dom';
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { toast } from 'react-toastify'
+import { UserAuthenticate } from '../../../services/authservice';
+
+const AddCategory: React.FC = () => {
+    let navigate: NavigateFunction = useNavigate();
+    const [successful, setSuccessful] = useState<boolean>(false);
+
+    const sleep = () => new Promise(r => setTimeout(r, 2000));
+
+    //Init da instance
+    const initialValues: any = {
+        category: ''
+    };
+
+    const validationSchema = Yup.object().shape({
+        category: Yup.string().test("len", "Category name must contains 3-20 caracteres.",
+            (val: any) =>
+                val &&
+                val.toString().length >= 3 &&
+                val.toString().length <= 20
+        ).required("Required field!"),
+    });
+
+    const handleRegister = (formValue: any) => {
+        //AQUI ENVIOOOOO
+        UserAuthenticate(formValue).then(async (response) => {
+            toast.info(response.data);
+            setSuccessful(true);
+            await sleep();
+            navigate("/");
+            window.location.reload();
+        },
+            (error) => {
+                toast.info(error);
+                setSuccessful(false);
+            }
+        );
+    };
+
+    return (
+        <div className="col-md-12">
+            <div className="card card-container">
+                <h1>Register a new category!</h1>
+                {/* Validation */}
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}//Regras
+                    onSubmit={handleRegister}
+                >
+                    <Form>
+                        {!successful && (
+                            <div>
+                                <div className="form-group">
+                                    <label htmlFor="category"> Category Name </label>
+                                    <Field name="category" type="text" className="form-control" />
+                                    <ErrorMessage
+                                        name="category"
+                                        component="div"
+                                        className="alert alert-danger"
+                                    />
+                                </div>
+                                <div className="form-group m-1">
+                                    <button type="submit" className="btn btn-primary btn-block">Register</button>
+                                </div>
+                            </div>
+                        )}
+                    </Form>
+                </Formik>
+            </div>
+        </div>
+    );
+};
+
+export default AddCategory;
---
+++ frontend/my-app/src/pages/Add/Category/index.tsx
@@ -0,0 +1,15 @@
+import AddCategory from '../../../components/add/addcategory';
+import Footer from '../../../components/footer/index';
+
+
+function AddCategoryPage() {
+    return (
+        <>
+            <AddCategory />
+            <Footer />  
+        </>
+    );
+}
+
+
+export default AddCategoryPage
