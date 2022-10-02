aa59973c0cf66d71ad3565ed62a32f59d3d87bc8
falchizao <marcelonavarro@alunos.utfpr.edu.br>
2022-10-02 00:26:27 -0300

    refactor: authservice

--- frontend/my-app/src/Routes.tsx
+++ frontend/my-app/src/Routes.tsx
@@ -2,7 +2,7 @@ import Category from './pages/CategoryPage';
 import Account from './pages/AccountPage';
 import Movimentation from './pages/MovimentationPage';
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/Loginpage';
 
@@ -10,19 +10,19 @@ function WebRoutes() {
     return (
         <BrowserRouter>
             <Routes >
-                <Route path="/Home" element={<Home />  } >    
+                <Route path="/" element={<LoginPage />}>
                 </Route>
-                <Route path="/Category" element={<Category />}>                                         
+                <Route path="/Register" element={<RegisterPage />}>
                 </Route>
-                <Route path="/Account" element={<Account />}>                                         
+                <Route path="/Home" element={<Home />} >
                 </Route>
-                <Route path="/Movimentation" element={<Movimentation />}>                                         
+                <Route path="/Category" element={<Category />}>
                 </Route>
-                <Route path="/" element={<LoginPage />}>                                         
+                <Route path="/Account" element={<Account />}>
                 </Route>
-                <Route path="/Register" element={<RegisterPage />}>                                         
+                <Route path="/Movimentation" element={<Movimentation />}>
                 </Route>
-            </Routes>        
+            </Routes>
         </BrowserRouter>
     );
 }
--- frontend/my-app/src/services/authservice.ts
+++ frontend/my-app/src/services/authservice.ts
@@ -1,63 +1,73 @@
 import customAxios from "../myAxios/myCustomAxios";
 import userTokenAuth from "./dataservice";
-const route = "localhost:8085/api";
+const route = "http://localhost:8085/api";
 
 //Security
-export async function UserAuthenticate(authForm : any) {
-    return await customAxios.post(`/${route}/authorization/register`, authForm);
+export async function UserAuthenticate(authForm: any) {
+    return await customAxios.post(`${route}/authorization/register`, authForm);
 }
-export async function UserLogin(loginCredentials : any) {
-    return await customAxios.post(`/${route}/authorization/login`, loginCredentials)
-    .then((response) => {
-        if(response.data.accessToken){
-            localStorage.setItem('@FinanceWeb::user', JSON.stringify(response.data));
-        }
-    });
+export async function UserLogin(loginCredentials: any) {
+    console.log(loginCredentials, "< - credentials")
+    return await customAxios.post(`${route}/authorization/login`, loginCredentials)
+        .then((response) => {
+            if (response.data) {
+                localStorage.setItem('@FinanceWeb::user', JSON.stringify(response.data));
+            }
+        });
 }
 
 //Accounts
-export async function GetAllAccounts(id: any) {
-    return await customAxios.get(`/${route}/account`, { headers: userTokenAuth() });
+export async function GetAllAccounts() {
+    return await customAxios.get(`${route}/account`, { headers: userTokenAuth() });
 }
 export async function GetAccount(id: any) {
-    return await customAxios.get(`/${route}/account/${id}`, { headers: userTokenAuth() });
+    return await customAxios.get(`${route}/account/${id}`, { headers: userTokenAuth() });
 }
 export async function AddAccount() {
-    return await customAxios.delete(`/${route}/account/registrar`, { headers: userTokenAuth() });
+    return await customAxios.delete(`${route}/account/registrar`, { headers: userTokenAuth() });
 }
-export async function DeleteAccount(id : any) {
-    return await customAxios.delete(`/${route}/account/${id}, { headers: userTokenAuth() }`);
+export async function DeleteAccount(id: any) {
+    return await customAxios.delete(`${route}/account/${id}`, { headers: userTokenAuth() });
 }
-export async function UpdateAccount(id : any) {
-    return await customAxios.put(`/${route}/account/${id}, { headers: userTokenAuth() }`);
+export async function UpdateAccount(id: any) {
+    return await customAxios.put(`${route}/account/${id}`, { headers: userTokenAuth() });
 }
 
 //Categories
 export async function GetCategories() {
-    return await customAxios.get(`/${route}/category`, { headers: userTokenAuth() });
+    return await customAxios.get(`${route}/category`, { headers: userTokenAuth() });
 }
 export async function GetCategory(id: any) {
-    return await customAxios.get(`/${route}/category/${id}`, { headers: userTokenAuth() });
+    return await customAxios.get(`${route}/category/${id}`, { headers: userTokenAuth() });
 }
 export async function AddCategory() {
-    return await customAxios.delete(`/${route}/category/registrar`, { headers: userTokenAuth() });
+    return await customAxios.delete(`${route}/category/registrar`, { headers: userTokenAuth() });
 }
-export async function DeleteCategory(id : any) {
-    return await customAxios.delete(`/${route}/category/${id}`, { headers: userTokenAuth() });
+export async function DeleteCategory(id: any) {
+    return await customAxios.delete(`${route}/category/${id}`, { headers: userTokenAuth() });
 }
-export async function UpdateCategory(id : any) {
-    return await customAxios.put(`/${route}/category/${id}`, { headers: userTokenAuth() });
+export async function UpdateCategory(id: any) {
+    return await customAxios.put(`${route}/category/${id}`, { headers: userTokenAuth() });
 }
 
 //Movimentations
 export async function GetMovimentations() {
-    return await customAxios.get(`/${route}/movimentation`, { headers: userTokenAuth() });
+    return await customAxios.get(`${route}/movimentation`, { headers: userTokenAuth() });
 }
 export async function GetMovimentation(id: any) {
-    return await customAxios.get(`/${route}/movimentation/${id}`, { headers: userTokenAuth() });
+    return await customAxios.get(`${route}/movimentation/${id}`, { headers: userTokenAuth() });
 }
 export async function AddMovimentations() {
-    return await customAxios.delete(`/${route}/movimentation/registrar`, { headers: userTokenAuth() });
+    return await customAxios.delete(`${route}/movimentation/registrar`, { headers: userTokenAuth() });
 }
+export async function PendingMovimentations(dmin: string, dmax: string){
+    return await customAxios.get(`${route}/movimentation/pending?minDate=${dmin}&maxDate=${dmax}`, { headers: userTokenAuth()});
+}
+
+//Logout
+export const Logout = () => {
+    localStorage.removeItem("@FinanceWeb::user");
+    window.location.reload();
+};
 
 
